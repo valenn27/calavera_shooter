2bee84e758102a3615996d1ee1a6aa5d71f98a0f
Valen <valentinopages664@gmail.com>
2024-06-14 08:46:48 -0300

    puse colores

--- src/main.py
+++ src/main.py
@@ -1,1 +1,27 @@
 import pygame
+from settings import * 
+
+pygame.init()
+
+
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('primer juego')
+
+
+
+SCREEN.fill((CUSTOM))
+
+contador = 0 
+continuar = True
+
+while continuar: 
+    print(contador)
+    contador += 1 
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT: 
+            continuar = False 
+
+
+    pygame.display.flip()
+
+pygame.quit()   
